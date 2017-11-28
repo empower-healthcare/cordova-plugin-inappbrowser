2bc8a52a2792936d334ee9c42788b65d0810102f
ToshiakiOshizawa <toshiaki.oshizawa@sbr-inc.co.jp>
2017-11-28 20:32:55 +0900

    useragentログ

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -440,7 +440,7 @@
         [self openInSystem:url];
         return NO;
     }
-    else if ((self.callbackId != nil) && (isTopLevelNavigation || [[url scheme] isEqualToString:@"bun2card"])) {
+    else if ((self.callbackId != nil) /*&& (isTopLevelNavigation || [[url scheme] isEqualToString:@"bun2card"])*/) {
         // Send a loadstart event for each top-level navigation (includes redirects).
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
                                                       messageAsDictionary:@{@"type":@"loadstart", @"url":[self.inAppBrowserViewController userAgent]/*[url absoluteString]*/}];
